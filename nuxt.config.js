e9efb942655a7d3cf3b93e14168c6616491baa81
sakusaku000 <harry.spncr@gmail.com>
2022-06-10 19:20:16 +0100

    initial

---
+++ nuxt.config.js
@@ -0,0 +1,22 @@
+export default {
+  head: {
+    title: 'saku',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  components: true,
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  target: 'static'
+}
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>lol</h1>
+</template>
