fb92fc9cddff5e07c8acd128761b89605b70f02a
sakusaku000 <harry.spncr@gmail.com>
2022-06-13 00:32:52 +0100

    changed wording on bannerista project

--- components/Work/Projects/Bannerista.vue
+++ components/Work/Projects/Bannerista.vue
@@ -1,7 +1,7 @@
 <template>
     <WorkProjectCard name="bannerista" icon="https://api.saku.dev/bannerista/banner/grid" years="2020" language="discord" bgColour="#95cafe">
         <p>
-            a discord bot created for the 95 degrees discord community that tracks text and voice activity and creates 
+            a discord bot created for a server that tracks text and voice activity and creates 
             a daily leaderboard of most to least active members in the form of a grid of avatars which can be used as the server's
             banner image.
         </p>
