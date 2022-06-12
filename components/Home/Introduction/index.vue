c689147a36b7661d0663e6c93cef2f9740b7023e
sakusaku000 <harry.spncr@gmail.com>
2022-06-12 16:20:49 +0100

    updated introduction

--- components/Home/Introduction/index.vue
+++ components/Home/Introduction/index.vue
@@ -1,14 +1,17 @@
 <template>
     <section id="introduction" class="lg:max-w-2xl mb-4">
-        <h2>hi there,</h2>
+        <h2>hi there 👋</h2>
         <p>
-            i'm saku, real name harry, and i'm a web dev based in london, uk.
+            i'm saku, real name harry, and i'm a web dev based in london, england.
         </p>
         <p>
-            i'm 21 and my pronouns are he/him.
+            currently 21 y/o and my pronouns are he/him.
         </p>
         <p>
-            i've been creating websites, web apps, bots and services since 2017 both in my free time as a hobby and for work.
+            since 2017 i've been creating and contributing to websites / web apps, bots and services both in my free time as a hobby and for work. 
+        </p>
+        <p>
+            away from the web stuff, i'm super into <router-link to="/kpop" class="underline">k-pop</router-link>, anime and games!
         </p>
     </section>
 </template>
