1655b083b7812c8e0ac4cc258e28e0955124c4a5
sakusaku000 <harry.spncr@gmail.com>
2022-06-13 00:30:06 +0100

    added saku.dev to work page

---
+++ components/Work/Projects/SakuDev.vue
@@ -0,0 +1,11 @@
+<template>
+    <WorkProjectCard name="saku.dev" :icon="require('~/assets/image/projects/sakudev/thumbnail.png')" years="2022" language="vue" bgColour="#ffc6ff">
+        <p>
+            the site you're currently looking at! this is my personal website with stuff about me, my interests and some of my projects.
+        </p>
+        <p class="mb-0">
+            built with nuxt.js. deployed on vercel.
+        </p>
+        <WorkProjectLinkOut link="https://github.com/sakusaku000/topify">view on github</WorkProjectLinkOut>
+    </WorkProjectCard>
+</template>
