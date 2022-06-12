11e78c4f32dfb309039826b309aaf71aef2b13f1
sakusaku000 <harry.spncr@gmail.com>
2022-06-13 00:30:50 +0100

    added info about what some projects were built with

--- components/Work/Projects/BanneristaDash.vue
+++ components/Work/Projects/BanneristaDash.vue
@@ -1,8 +1,11 @@
 <template>
-    <WorkProjectCard name="bannerista dash" :icon="require('~/assets/image/projects/bannerista-dash/thumbnail.png')" years="2021" language="vue">
-        <p class="mb-0">
-            management dashboard built in vue.js that interacts with the main bannerista bot. allows for uploading and selection of banner styles, leaderboard data management
+    <WorkProjectCard name="bannerista dash" :icon="require('~/assets/image/projects/bannerista-dash/thumbnail.png')" years="2021" language="vue" bgColour="#CDCDCD">
+        <p>
+            management dashboard that interacts with the main bannerista bot. allows for uploading and selection of banner styles, leaderboard data management
             and controlling various settings.
+        </p>
+        <p class="mb-0">
+            built with vue.js and deployed on vercel.
         </p>
     </WorkProjectCard>
 </template>
--- components/Work/Projects/Topify.vue
+++ components/Work/Projects/Topify.vue
@@ -1,10 +1,14 @@
 <template>
-    <WorkProjectCard name="topify" :icon="require('~/assets/image/projects/topify/thumbnail.png')" years="2021" language="vue">
+    <WorkProjectCard name="topify" :icon="require('~/assets/image/projects/topify/thumbnail.png')" years="2021" language="vue" bgColour="#10b981">
         <p>
             small web app that shows top data from your spotify account. allows for viewing top tracks and artists within a selectable time range.
         </p>
-        <p class="mb-0">
-            currently private because spotify api requires a whitelist 😑 
+        <p>
+            live site currently private because spotify api requires a whitelist 😑 
         </p>
+        <p class="mb-0">
+            built with vue.js and spotify's API. deployed on vercel.
+        </p>
+        <WorkProjectLinkOut link="https://github.com/sakusaku000/topify">view on github</WorkProjectLinkOut>
     </WorkProjectCard>
 </template>
