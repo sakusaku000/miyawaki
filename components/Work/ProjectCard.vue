ebe2556b9582889ff3724d505ca7291e32f1315b
sakusaku000 <harry.spncr@gmail.com>
2022-06-13 17:31:40 +0100

    fixed alt text on project cards

--- components/Work/ProjectCard.vue
+++ components/Work/ProjectCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-10 mb-20 items-center md:items-start text-justify">
         <div class="hidden lg:block px-5 pt-5 rounded-2xl" id="card-thumbnail" :style="`background-color:${bgColour};`">
-            <img class="w-full rounded-x-2xl rounded-t-2xl bg-shadow" :src="icon" :alt="`${name} thumbnail`" draggable="false">
+            <img class="w-full rounded-x-2xl rounded-t-2xl bg-shadow" :src="icon" :alt="`${name} cover image`" draggable="false">
         </div>
         <div id="card-content" class="bg-saku-dark-100 p-8 rounded-2xl">
             <h3>{{years}}</h3>
             <h1><WorkProjectLanguageIcon :language="language"/>{{name}}</h1>
-            <img class="w-full rounded-lg block lg:hidden mb-4" :src="icon" alt="Bannerista Logo" draggable="false">
+            <img class="w-full rounded-lg block lg:hidden mb-4" :src="icon" :alt="`${name} cover image`" draggable="false">
             <div class="lg:max-w-2xl">
                 <slot/>
             </div>
